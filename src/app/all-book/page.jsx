a66dd82780fe3b657c7e0d2a3531078d11bd9547
Jony-Mia <ajonymia321@gmail.com>
2026-05-01 18:30:30 +0600

    all book section creation started

--- src/app/all-book/page.jsx
+++ src/app/all-book/page.jsx
@@ -1,19 +1,23 @@
 import Search from "@/component/Search"
 import { nunito, poppins } from "../layout";
+import { FilterCheckbox } from "@/component/FilterCheckbox";
 
 const page = () => {
-    
+
     return (
         <div className="bg-[#f9f6f1bc]">
-            <h1 className={`${nunito.className} text-center pt-8 font-semibold text-4xl`}>All Book</h1>
-            <p className={`${poppins.className} text-center text-md mt-5 text-muted`}>Search the catalog or filter by category to find your next read.</p>
-            <div>
-                <Search />
-                <br />
-            </div>
-            <div>
-
-
+            <div className="container mx-auto">
+                <h1 className={`${nunito.className} text-center pt-8 font-semibold text-4xl`}>All Book</h1>
+                <p className={`${poppins.className} text-center text-md mt-5 text-muted`}>Search the catalog or filter by category to find your next read.</p>
+                <div>
+                    <Search />
+                    <br />
+                </div>
+                <div>
+                    <div>
+                        <FilterCheckbox />
+                    </div>
+                </div>
             </div>
         </div>
     )
